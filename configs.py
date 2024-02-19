1e15115b8f547d755900cbe70ebbc462fddc7c5c
MailBot303 <159624210+MailBot303@users.noreply.github.com>
2024-02-19 13:29:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
     BOT_TOKEN = getenv("BOT_TOKEN", "6478248479:AAGE_WN9bNrtEo9TPiVpkDfvpkVp5EJSNiM")
     FSUB = getenv("FSUB", "TheFunkyGroup")
-    CHID = int(getenv("CHID", "-1002000671343"))
+    CHID = int(getenv("CHID", "-1001596583384"))
     SUDO = list(map(int, getenv("SUDO", "6643862052").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://lostzoro567:luCu7z2IQyIoZ1kR@cluster0.hkq0krw.mongodb.net/?retryWrites=true&w=majority")
     
