b368ebb96ea99194f94171c5221207688098d41d
MailBot303 <159624210+MailBot303@users.noreply.github.com>
2024-02-19 06:50:39 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 
 from os import path, getenv
 
@@ -9,13 +5,9 @@ class Config:
     API_ID = int(getenv("API_ID", "20389440"))
     API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
     BOT_TOKEN = getenv("BOT_TOKEN", "6478248479:AAGE_WN9bNrtEo9TPiVpkDfvpkVp5EJSNiM")
-    FSUB = getenv("FSUB", "VJ_Botz")
+    FSUB = getenv("FSUB", "TheFunkyGroup")
     CHID = int(getenv("CHID", "-1002000671343"))
     SUDO = list(map(int, getenv("SUDO", "6643862052").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://lostzoro567:luCu7z2IQyIoZ1kR@cluster0.hkq0krw.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
-
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
