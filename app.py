0573cff73ae97588289bcc1db1ee3fd7cd7407f4
MailBot303 <159624210+MailBot303@users.noreply.github.com>
2024-02-19 06:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ'
+    return 'TheBotIsAlive'
 
 
 if __name__ == "__main__":
     app.run()
-
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
